6e7b1a5301913609dc2acc6ac52311a3b3071cc9
WeeblyMon <gurugaryc@gmail.com>
2024-04-06 23:17:38 +0100

    draft

--- Globals.lua
+++ Globals.lua
@@ -21,6 +21,11 @@ function toggleInitiativeList()
   UI.setAttribute("initiativeListMenu", "active", tostring(not isActive))
 end
 
+function toggleCooldownAdderMenu()
+  local isActive = UI.getAttribute("AddCoolDownMenu", "active") == "true"
+  UI.setAttribute("AddCoolDownMenu", "active", tostring(not isActive))
+end
+
 function registerPlayer(player, value, id)
   -- Placeholder for now; put registration logic here later
   print("Register Player button clicked")
@@ -37,4 +42,8 @@ end
 
 function closeInitiativeMenu2()
   UI.setAttribute("enemyInitiativeMenu", "active", "false")
+end
+
+function closeInitiativeMenu3()
+  UI.setAttribute("AddCoolDownMenu", "active", "false")
 end
